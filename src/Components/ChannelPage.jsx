d3b463a11f55ac5df4f4f7a6dfb4432cb1f0f67e
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-21 14:07:54 +0530

    Add avatar upload functionality to ChannelPage.jsx

--- src/Components/ChannelPage.jsx
+++ src/Components/ChannelPage.jsx
@@ -53,10 +53,12 @@ function ChannelPage() {
     let flag1=true;
     let flag2=true;
     let flag3=true;
+
     if(avatar.value){
       console.log(avatar.value);
+      console.log(avatar.files);
       const formData = new FormData();
-      formData.append('avatar', avatar.files[0])
+      formData.append('avatar', avatar?.files[0])
       console.log(avatar.files[0]);
       console.log(formData);
       const data = await axios.patch(`/api/v1/users/update-avatar`, formData)
